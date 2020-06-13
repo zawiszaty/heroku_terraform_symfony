1798d2b32e3800ceef1ec1c7d0ff28061e5f3f9b
zawiszaty <ZawiszaCzarny52@gmail.com>
2020-06-13 17:17:02 +0200

    cirleci

--- .circleci/setup-terraform.sh
+++ .circleci/setup-terraform.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ~/bin
 cd ~bin
-wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
+sudo wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 
-unzip terraform_0.12.26_linux_amd64.zip
+sudo unzip terraform_0.12.26_linux_amd64.zip
 
 terraform -v
