282616a9e5388a1150efe48c42f587815dbc9777
zawiszaty <ZawiszaCzarny52@gmail.com>
2020-06-13 17:03:41 +0200

    cirleci

---
+++ .circleci/setup-terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir ~/bin
+
+wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.26_linux_amd64.zip
+
+unzip terraform_0.12.24_linux_amd64.zip
+
+mv terraform ~/bin
+
+terraform -v
